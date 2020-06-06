ccbd3c51c9c5baca923a0dbbba69acc10811763e
Jonathan Burckel <jonathan.burckel@gmail.com>
2020-06-05 18:25:27 -1000

    changes in products import several helper

--- mpapi/api/v1/routers/products.py
+++ mpapi/api/v1/routers/products.py
@@ -1,13 +1,15 @@
-from fastapi import APIRouter, HTTPException, Path
+from fastapi import APIRouter, HTTPException, Path, Depends
 from typing import List
 from bson import ObjectId, errors
 
-from mpapi.database import db
+from mpapi.core.database import get_collection
+from mpapi.crud.users import get_current_user
+from mpapi.schemas.users import User
 from mpapi.schemas.products import ProductCreate, Product
 
 router = APIRouter()
 
-products = db.products
+products = get_collection("products")
 
 @router.get("/", response_model=List[Product])
 async def get_products(skip: int = 0, limit: int = 100):
@@ -27,8 +29,9 @@ def get_product_by_id(product_id: str = Path(..., title="The product ID as a val
         raise HTTPException(status_code=404, detail="Product not found")
     return product
 
+
 @router.post("/", response_model=Product)
-def get_product_by_id(product: ProductCreate):
+def create_product(product: ProductCreate, current_user: User = Depends(get_current_user)):
     try:
         product_id = products.insert_one(product.dict()).inserted_id
         db_product = products.find_one({'_id': ObjectId(product_id)})
