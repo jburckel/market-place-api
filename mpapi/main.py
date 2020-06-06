8d2656285c997b3a7a66021b838c039916f544a6
Jonathan Burckel <jonathan.burckel@gmail.com>
2020-06-05 18:23:04 -1000

    Changes required by token move to root

--- mpapi/main.py
+++ mpapi/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-from mpapi import home
-from mpapi import api
+from mpapi import api, home, token
 
 app = FastAPI()
 
 app.include_router(home.router)
+
+app.include_router(token.router)
 
 app.include_router(
     api.v1.router,
