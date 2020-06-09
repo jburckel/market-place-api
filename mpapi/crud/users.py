0ee0124f099d8b3ff2092d1e2656ef6009dd2abe
Jonathan Burckel <jonathan.burckel@gmail.com>
2020-06-08 15:28:40 -1000

    Create crud for users

--- mpapi/crud/users.py
+++ mpapi/crud/users.py
@@ -2,19 +2,25 @@ from bson import ObjectId
 
 from fastapi import HTTPException, Depends, status
 
-from mpapi.core.auth import oauth2_scheme, decode_access_token
+from mpapi.core.auth import oauth2_scheme, decode_access_token, get_password_hash
 from mpapi.core.database import get_collection
-from mpapi.crud.helpers import find_one_by_id, find_one_by_value
+from mpapi.crud.helpers import find_one_by_id, find_one_by_value, insert_one
+from mpapi.schemas.users import UserIn, UserInDB
 
 
 users = get_collection("users")
 
-
-def get_user_by_id(user_id: str):
+def get_user_by_id(user_id: str) -> dict:
     return find_one_by_id(users, user_id)
 
-def get_user_by_username(username: str):
+
+def get_user_by_username(username: str) -> dict:
     return find_one_by_value(users, 'username', username)
+
+
+def get_users(skip: int = 0, limit: int = 0, filters: dict = None) -> list:
+    return [user for user in users.find(filters).skip(skip).limit(limit)]
+
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
@@ -29,3 +35,9 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
     if user is None:
         raise credentials_exception
     return user
+
+
+def create_user(user: UserIn) -> str:
+    hashed_password = get_password_hash(user.password)
+    user_in_db = UserInDB(**user.dict(), hashed_password=hashed_password)
+    return insert_one(users, user_in_db.dict())
