b6238308e0b324239ff3437c71e80fed90ada82e
Jonathan Burckel <jonathan.burckel@gmail.com>
2020-06-08 15:28:22 -1000

    Using crud inside routes

--- mpapi/api/v1/routers/products.py
+++ mpapi/api/v1/routers/products.py
@@ -1,28 +1,24 @@
-from fastapi import APIRouter, HTTPException, Path, Depends
+from fastapi import APIRouter, Path, Depends
 from typing import List
-from bson import ObjectId, errors
 
-from mpapi.core.database import get_collection
 import mpapi.crud.products as crud
 from mpapi.crud.users import get_current_user
-from mpapi.schemas.users import User
-from mpapi.schemas.products import ProductCreate, Product
+from mpapi.schemas.users import UserOut
+from mpapi.schemas.products import ProductIn, ProductOut
 
 router = APIRouter()
 
-products = get_collection("products")
-
-@router.get("/", response_model=List[Product])
+@router.get("/", response_model=List[ProductOut])
 async def get_products(skip: int = 0, limit: int = 100):
     return crud.get_products(skip, limit)
 
 
-@router.get("/{product_id}", response_model=Product)
+@router.get("/{product_id}", response_model=ProductOut)
 def get_product_by_id(product_id: str = Path(..., title="The product ID as a valid ObjectId")):
     return crud.get_product_by_id(product_id)
 
 
-@router.post("/", response_model=Product)
-def create_product(product: ProductCreate, current_user: User = Depends(get_current_user)):
+@router.post("/", response_model=ProductOut)
+def create_product(product: ProductIn, current_user: UserOut = Depends(get_current_user)):
     product_id = crud.create_product(product.dict())
     return crud.get_product_by_id(product_id)
