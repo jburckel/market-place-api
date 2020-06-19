8e8dea79c7a9964c9d26fb0356381af4ec107dcb
Jonathan Burckel <jonathan.burckel@gmail.com>
2020-06-18 15:42:21 -1000

    Create pydantic class for reusing it in various schemas

--- mpapi/schemas/commons.py
+++ mpapi/schemas/commons.py
@@ -1,11 +1,55 @@
 from pydantic import BaseModel
 from typing import Optional
+from bson import ObjectId
+
+
+class ObjectIdStr(str):
+    """
+    id from MongoDB need to be valid ObjectId
+    """
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(str(v)):
+            return ValueError(f"Not a valid ObjectId: {v}")
+        return str(v)
+
+
+class CombinedOjectIdStr(str):
+    """
+    Combined Id this a combinaison of two ObjectIds seperate by =
+    """
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        objid1, objid2 = v.split("=")
+        if not ObjectId.is_valid(str(objid1)) or not ObjectId.is_valid(str(objid2)):
+            return ValueError(f"Not a valid ObjectId: {v}")
+        return str(v)
+
 
 class MultiLanguageText(BaseModel):
     en: str = None
     es: str = None
     fr: str = None
 
+
+class Address(BaseModel):
+    name: str = None
+    addressLine1: str
+    addressLine2: str = None
+    postalCode: str = None
+    town: str = None
+    state: str = None
+    country: str
+
+
 class Image(BaseModel):
     url: str
-    name: MultiLanguageText
+    description: MultiLanguageText = None
