e2d266fbf6ae5d8d2d191e7bf64dd90a4f751e66
Jonathan Burckel <jonathan.burckel@gmail.com>
2020-06-18 15:43:48 -1000

    Add some fields to products schema

--- mpapi/schemas/products.py
+++ mpapi/schemas/products.py
@@ -2,45 +2,53 @@ from pydantic import BaseModel
 from typing import List
 
 from .mixins import DBModel
-from .commons import MultiLanguageText, Image
+from .commons import MultiLanguageText, Image, ObjectIdStr, CombinedOjectIdStr
+from .product_attributes import ProductAttributeId
+from .sellers import SellerOut
 
-class Variant(BaseModel):
-    name: MultiLanguageText = None
-    price: float = None
 
-class ProductBase(BaseModel):
+class ProductTranslations(BaseModel):
     name: MultiLanguageText = None
     description: MultiLanguageText = None
+    short_description: MultiLanguageText = None
+
+
+class ProductBase(BaseModel):
+    productModelId: str = None #if None it's the productModel // is it better than a variant collection ???
+    name: str = None
+    translations: ProductTranslations = None
     sku: str = None
     mpn: str = None
     price: float = None
     priceCurrency: str = None
-    short_description: MultiLanguageText = None
-    image: List[Image] = None
-    variant: List[Variant] = None
+    images: List[Image] = None
+    attributes: List[ProductAttributeId] = None
+    secondaryAttributes: List[ProductAttributeId] = None
+    sellerId: ObjectIdStr = None
+    active: bool = None
 
 
 class ProductToInsert(ProductBase):
     """
-        When receiving a new product to insert
-        Some fields are required
+        When receiving a new product to insert, some fields are required and to
+        define some default values
     """
-    code: str
-    price: float
+    name: str
+    active: bool = False
+    sellerId: ObjectIdStr
     pass
 
 
 class ProductToUpdate(ProductBase):
     """
-        When receiving a product to update
-        It may be partial so every fields are optional
-        Used for validation and to remove unwanted values
+        When receiving a product to update, it may be partial so every fields
+        are optional. Used for validation and to remove unwanted values
     """
     pass
 
 
 class ProductOut(DBModel, ProductBase):
     """
-        When sending a product, we must include _id
+        When sending a product, we must include _id.
     """
     pass
