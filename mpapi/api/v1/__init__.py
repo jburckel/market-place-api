cf3ad9cb34a22298051200132c3dd98dec514575
Jonathan Burckel <jonathan.burckel@gmail.com>
2020-06-19 17:25:00 -1000

    Modified tags on routes

--- mpapi/api/v1/__init__.py
+++ mpapi/api/v1/__init__.py
@@ -1,17 +1,26 @@
 from fastapi import APIRouter
 
-from .routers import products, users
+from .routers import products, sellers, users
 
 router = APIRouter()
 
 router.include_router(
     products.router,
-    prefix="/products",
+    prefix='/products',
     tags=['products']
 )
 
+
+router.include_router(
+    sellers.router,
+    prefix='/sellers',
+    tags=['sellers']
+)
+
+
+
 router.include_router(
     users.router,
-    prefix="/users",
+    prefix='/users',
     tags=['users']
 )
