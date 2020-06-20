4ec191262858c9559b0153b356bd4d00639b7d0a
Jonathan Burckel <jonathan.burckel@gmail.com>
2020-06-19 17:24:11 -1000

    Changing setting

--- mpapi/core/collection.py
+++ mpapi/core/collection.py
@@ -2,14 +2,17 @@ from pymongo import ASCENDING
 
 COLLECTIONS = {
     "PRODUCTS": "products",
-    "USERS": "users"
+    "USERS": "users",
+    "SELLERS": "sellers"
 }
 
 def create_collections(db):
 
     products = db.create_collection(COLLECTIONS["PRODUCTS"])
-    products.create_index([('sku', ASCENDING)], unique=True)
+    products.create_index([('sku', ASCENDING)])
     products.create_index([('productModelId', ASCENDING)])
 
     users = db.create_collection(COLLECTIONS["USERS"])
     users.create_index([('username', ASCENDING)], unique=True)
+
+    sellers = db.create_collection(COLLECTIONS["SELLERS"])
--- mpapi/core/database.py
+++ mpapi/core/database.py
@@ -1,13 +1,12 @@
 from pymongo import MongoClient, errors
 
 from mpapi import settings
-from .collection import create_collections
 
-serverTimeout = 2000
+from .collection import create_collections
 
 try:
     if not(settings.DATABASE_USERNAME):
-        client = MongoClient(settings.DATABASE_URI, serverSelectionTimeoutMS = serverTimeout)
+        client = MongoClient(settings.DATABASE_URI, serverSelectionTimeoutMS = settings.DATABASE_TIMEOUT)
     else:
         client = MongoClient(
             settings.DATABASE_URI,
@@ -15,7 +14,7 @@ try:
             password = settings.DATABASE_PASSWORD,
             authSource = settings.DATABASE_AUTHSOURCE,
             authMechanism = settings.DATABASE_AUTHMECHANISM,
-            servertSelectionTimeoutMS = serverTimeout
+            servertSelectionTimeoutMS = settings.SERVER_TIMEOUT
         )
 
     info = client.server_info()
@@ -30,6 +29,7 @@ except AttributeError as error:
 if settings.DATABASE_NAME not in client.list_database_names():
     print('Creating collections')
     create_collections(db)
+
 
 def get_collection(collection: str):
     try:
