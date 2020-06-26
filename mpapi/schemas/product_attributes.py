554f57b865317bc9e66b4cb17e5545d9007a2bd7
Jonathan Burckel <jonathan.burckel@gmail.com>
2020-06-26 09:44:31 -1000

    Create Product Attribute Value schema

--- mpapi/schemas/product_attributes.py
+++ mpapi/schemas/product_attributes.py
@@ -1,17 +1,72 @@
-from pydantic import BaseModel, Field
 from typing import Optional
-from .commons import ObjectIdStr, MultiLanguageText
+from pydantic import BaseModel, Field
 
+from ._commons import ObjectIdStr, MultiLanguageText
+from ._mixins import DBModel
 
-class ProductAttributeId(BaseModel):
-    id: Optional[ObjectIdStr] = Field(..., alias='_id')
-    pass
+#
+#
+# Product Attribute
+#
+#
 
 
 class ProductAttributeTranslations(BaseModel):
-    name: MultiLanguageText
-    value: MultiLanguageText
+    name: MultiLanguageText = None
 
 
-class ProductAttribute(ProductAttributeId):
+class ProductAttributeBase(BaseModel):
+    name: str = None
     translations: ProductAttributeTranslations = None
+    sellerId: ObjectIdStr = None
+
+
+class ProductAttributeToInsert(ProductAttributeBase):
+    name: str
+    sellerId: ObjectIdStr
+    pass
+
+
+class ProductAttributeToUpdate(ProductAttributeBase):
+    @validator('sellerId')
+    def prevent_none(cls, v):
+        assert v is not None, 'sellerId may not be None'
+        return v
+
+
+class ProductAttributeOut(DBModel, ProductAttributeBase):
+    pass
+
+
+#
+#
+# Product Attribute Value
+#
+#
+
+
+class ProductAttributeValueTranslations(BaseModel):
+    name: MultiLanguageText = None
+
+
+class ProductAttributeValueBase(BaseModel):
+    name: str = None
+    translations: ProductAttributeValueTranslations = None
+    productAttributeId: ObjectIdStr = None
+
+
+class ProductAttributeValueToInsert(BaseModel):
+    name: str
+    productAttributeId: ObjectIdStr
+    pass
+
+
+class ProductAttributeValueToUpdate(BaseModel):
+    @validator('productAttributeId')
+    def prevent_none(cls, v):
+        assert v is not None, 'productAttributeId may not be None'
+        return v
+
+
+class ProductAttributeValueOut(DBModel, ProductAttributeValueBase):
+    pass
