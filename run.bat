442f7e7417558a0ec2fd99388f575b3e52004bba
Jonathan Burckel <jonathan.burckel@gmail.com>
2020-06-26 10:19:09 -1000

    add run test

--- run.bat
+++ run.bat
@@ -3,9 +3,15 @@ IF "%1" == "prod" GOTO prod
 IF "%1" == "PROD" GOTO prod
 IF "%1" == "dev" GOTO dev
 IF "%1" == "DEV" GOTO dev
+IF "%1" == "test" GOTO test
+IF "%1" == "TEST" GOTO test
 
 :prod
 uvicorn mpapi.main:app
 
 :dev
 uvicorn mpapi.main:app --reload
+
+:test
+pytest
+flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics --exclude venv,.git
--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 if [ "$1" = "dev" ] | [ "$1" = "DEV" ]; then
- uvicorn mpapi.main:app --reload
+  uvicorn mpapi.main:app --reload
+elif [ "$1" = "test" ] | [ "$1" = "TEST" ]; then
+  pytest
+  flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics --exclude venv,.git
 else
   uvicorn mpapi.main:app
 fi
